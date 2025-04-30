41304ecaf1ad5e3139e95cd791d22910e09278ae
Arbaj Ansari <127431775+ArbajAnsari19@users.noreply.github.com>
2025-04-30 16:39:12 +0530

    Update index.ts

--- Eatoes_backend/src/index.ts
+++ Eatoes_backend/src/index.ts
@@ -37,6 +37,4 @@ app.get('/test', (req, res) => {
 
 // Start server
 // Only listen to port if not in Vercel environment
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
